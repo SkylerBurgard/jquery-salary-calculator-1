149a531b270440ea22ca26c94bea571ef0f8e2fa
Skyler Burgard <skylerrburgard@hotmail.com>
2020-06-26 16:46:07 -0500

    Starting submitEmployee function - getting undefiend in console currently

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 $(document).ready(onReady);
+
+const monthlyBudget = 20000;
+
+function onReady() {
+    $('#js-salary-calc-form').on('submit', submitEmployee);
+
+    // render();
+}
+
+function submitEmployee(event) {
+    event.preventDefault();
+
+    const submitEmployee = {
+        firstName: $('js-input-firstName').val(),
+        lastName: $('js-input-lastName').val(),
+        employeeID: $('js-input-employeeID').val(),
+        jobTitle: $('js-input-jobTitle').val(),
+        employeeAnnualSalary: $('js-input-employeeAnnualSalary').val()
+    };
+    console.log(submitEmployee);
+}
